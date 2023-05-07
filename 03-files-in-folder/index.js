6ce720fc9b0af55c8218dc9be87dc3c0283608a0
Phoeni10 <webjscreator@gmail.com>
2023-05-08 01:30:35 +0300

    feat:solve 03-files-in-folder (08.05.2023 01:30:21)

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const obj = fs.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true});
+
+obj.then(files => files.forEach(file => {
+    if (file.isFile()) {
+        const pathToFile = path.join(__dirname, 'secret-folder', file.name);
+        const baseName = path.basename(pathToFile);
+        const extName = path.extname(pathToFile);
+        const stats = fs.stat(pathToFile);
+        
+        stats.then(stat => {
+            const size = stat.size;
+            const name = baseName.slice(0, baseName.indexOf('.'));
+            const ext = extName.slice(extName.indexOf('.') + 1);
+            console.log(`${name} - ${ext} - ${size/1024}kb`);
+        })
+    }
+}))
+
